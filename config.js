dd88c8eb360f30bb56be4f7de4a7d8f129c38940
Esteban Segura Benavides <esteban.1703@gmail.com>
2018-07-06 19:08:26 -0600

    Init commit

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+
+    'secret': 'SECRETPASSWORDKEY',
+    'database': 'mongodb://localhost:27017/CoffeShop',
+    'databaseProd': 'mongodb://essegura:hola123hola@ds113169.mlab.com:13169/comercioynegociosdigitales'
+
+};
---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var jwt = require('jsonwebtoken');
+var expressJwt = require('express-jwt');
+var session = require('cookie-session');
+
+var config = require('./config');
+
+if(process.env.NODE_ENV!=='production'){
+	//MORGAN USE FOR SEE BEAUTY REQUEST FOR THE SERVER
+	var morgan = require('morgan');
+	app.use(morgan('dev'));
+
+	mongoose.connect(config.database); 
+} else {
+	mongoose.connect(config.databaseProd); 
+}
+
+//BODY PARSER: MIDLEWARE USING FOR HANDLING BODY REQUEST
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+//CONFIGURE OUR APPLICATION
+var port = process.env.PORT || 3000; 
+
+app.listen(port,function(){
+	console.log("Running localhost port: "+port);
+})
